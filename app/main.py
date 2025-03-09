1b73de41ca1c6101da087ca8ded49c154472a7b5
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2025-03-09 16:01:03 +0530

    added logging responses for traiing tesnforflow

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
-from models import static_spacy, trained_spacy
-from trainers import training_spacy
+from models import static_spacy, trained_spacy, trained_tensorflow
+from trainers import training_spacy, training_tensorflow
 import json, os
 
 class App:
@@ -8,7 +8,9 @@ class App:
         self.app = Flask(__name__)
         self.static_model = static_spacy.Static_Model()
         self.dynamic_model = trained_spacy.Dynamic_Model()
+        self.tensor_model = trained_tensorflow.Tensor_Model()
         self.train_spacy = training_spacy.Training_Spacy()
+        self.train_tensor = training_tensorflow.Train_Tensor()
         self.setup_routes()
         self.intents_path = os.path.join(os.path.dirname(__file__), '.', 'data', 'intent_mapping.json')
 
@@ -23,13 +25,17 @@ class App:
             if not req_data or "input" not in req_data:
                 return jsonify({"error": "Missing 'input' parameter"}), 400
             msg = req_data["input"]
-            mode = req_data["mode"]
-            if mode == 1:
+            model = req_data["model"]
+            if model == 2:
+                response = self.tensor_model.predictIntent(msg)
+                mode = "TensorFlow"
+            elif model == 1:
                 response = self.dynamic_model.dynamic_response(msg)
+                mode = "Trained Spacy"
             else:
-                mode = 0
                 response = self.static_model.static_response(msg)
-            return jsonify({"reply": response, "mode": mode})
+                mode = "Static Spacy"
+            return jsonify({"reply": response, "model": mode})
 
         @self.app.route('/collect_feedback', methods=['POST'])
         def collect_feedback():
@@ -45,9 +51,18 @@ class App:
         
         @self.app.route('/train_spacy', methods=['GET'])
         def train_spacy_function():
-            epochs = request.args.get('n', default=10, type=int)
+            epochs = request.args.get('n', default=50, type=int)
             try:
                 result = self.train_spacy.train_model(epochs)
+                return jsonify(result)
+            except Exception as e:
+                return jsonify({"status": "failed", "error": str(e)})
+
+        @self.app.route('/train_tensor', methods=['GET'])
+        def train_tensor_function():
+            epochs = request.args.get('n', default=50, type=int)
+            try:
+                result = self.train_tensor.createModel(epochs)
                 return jsonify(result)
             except Exception as e:
                 return jsonify({"status": "failed", "error": str(e)})
